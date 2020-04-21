fca61e878786a6a798d558f877989699699cb0dd
Aaron <aaron.sadler@activeis.net>
2020-04-21 15:44:31 +0100

    Updated to work with gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,9 @@ var src = [
     dest = './MaintenanceMode.SiteV8/App_Plugins';
 
 gulp.task('monitor', function () {
-    for (var i = 0; i < src.length; i++) {
-        watch(src[i] + '/**/*', { ignoreInitial: false, verbose: true })
-            .pipe(gulp.dest(dest));
-    }
-
+    // Endless stream mode
+    return watch(src + '/**/*', { ignoreInitial: false, verbose: true })
+        .pipe(gulp.dest(dest));
 });
 
-gulp.task('default', ['monitor']);
+gulp.task('default', 'monitor');
