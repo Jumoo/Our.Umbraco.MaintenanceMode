dd955127d77f4b69685420df765737c7a6863c57
Aaron Sadler <aaron@aaronsadler.uk>
2022-02-24 14:57:52 +0000

    Added controller to handle maintenance mode view

---
+++ Our.Umbraco.MaintenanceModeV9/Controllers/MaintenanceModeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Our.Umbraco.MaintenanceModeV9.Interfaces;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Our.Umbraco.MaintenanceModeV9.Controllers
+{
+    public class MaintenanceModeController : SurfaceController
+    {
+        private readonly IMaintenanceModeService _maintenanceModeService;
+        [Route("maintain")]
+        public IActionResult Index()
+        {
+            return new ActionResults.ServiceUnavailableResult(_maintenanceModeService.Settings);
+        }
+
+        public MaintenanceModeController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, IMaintenanceModeService maintenanceModeService) 
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            _maintenanceModeService = maintenanceModeService;
+        }
+    }
+}
