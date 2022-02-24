6d73dfa8b2d5335e822e6a87e7e0ae3b2d79956a
Aaron Sadler <aaron@aaronsadler.uk>
2022-02-24 15:08:43 +0000

    Added runtime check

--- Our.Umbraco.MaintenanceModeV9/Middleware/MaintenanceRedirectMiddleware.cs
+++ Our.Umbraco.MaintenanceModeV9/Middleware/MaintenanceRedirectMiddleware.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-
 using Our.Umbraco.MaintenanceModeV9.Interfaces;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
 
 namespace Our.Umbraco.MaintenanceModeV9.Middleware
 {
@@ -17,36 +13,49 @@ namespace Our.Umbraco.MaintenanceModeV9.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<MaintenanceRedirectMiddleware> _logger;
         private readonly IMaintenanceModeService _maintenanceModeService;
+        //private readonly IBackofficeUserAccessor _backofficeUserAccessor;
+        private readonly IRuntimeState _runtimeState;
 
         public MaintenanceRedirectMiddleware(RequestDelegate next,
             ILogger<MaintenanceRedirectMiddleware> logger,
-            IMaintenanceModeService maintenanceService)
+            IMaintenanceModeService maintenanceService/*, IBackofficeUserAccessor backofficeUserAccessor*/,
+            IRuntimeState runtimeState)
         {
             _next = next;
             _logger = logger;
-            _maintenanceModeService = maintenanceService;  
+            _maintenanceModeService = maintenanceService;
+            //_backofficeUserAccessor = backofficeUserAccessor;
+            _runtimeState = runtimeState;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             _logger.LogInformation("Middleware triggered {url}", context.Request.Path);
 
-            if (_maintenanceModeService.IsInMaintenanceMode)
+            if (_runtimeState.Level == RuntimeLevel.Run &&
+                _maintenanceModeService.IsInMaintenanceMode)
             {
-                // do something...
-                var newPath = new PathString("/maintain");
+                // todo figure out how to do this check here
+                //if (!_maintenanceModeService.Settings.AllowBackOfficeUsersThrough
+                //    && !_backofficeUserAccessor.BackofficeUser.IsAuthenticated)
+                //{
+                    // do something...
+                    var newPath = new PathString("/maintain");
 
-                context.Features.Set<IStatusCodeReExecuteFeature>(new StatusCodeReExecuteFeature()
-                {
-                    OriginalPath = context.Request.Path,
-                    OriginalPathBase = context.Request.PathBase.Value,
-                    OriginalQueryString = context.Request.QueryString.HasValue 
-                        ? context.Request.QueryString.Value : null,
-                });
+                    context.Features.Set<IStatusCodeReExecuteFeature>(new StatusCodeReExecuteFeature()
+                    {
+                        OriginalPath = context.Request.Path,
+                        OriginalPathBase = context.Request.PathBase.Value,
+                        OriginalQueryString = context.Request.QueryString.HasValue
+                            ? context.Request.QueryString.Value
+                            : null,
+                    });
 
-                context.Request.Path = newPath;
+                    context.Request.Path = newPath;
+                //}
+
+                await _next(context);
             }
-            await _next(context);
         }
     }
 }
