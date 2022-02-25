9130ba08b8ff91b2d2b492719bdde40287503517
Kevin Jump <kevin@thejumps.co.uk>
2022-02-25 11:53:24 +0000

    package readyness.

---
+++ dist/buildpackage.ps1
@@ -0,0 +1,73 @@
+<#
+SYNOPSIS
+    Buils the optionally pushes the packages to the azure feed
+#>
+param (
+
+    [Parameter(Mandatory)]
+    [string]
+    [Alias("v")]  $version, #version to build
+
+    [Parameter()]
+    [string]
+    $suffix, # optional suffix to append to version (for pre-releases)
+
+    [Parameter()]
+    [string]
+    $env = 'release', #build environment to use when packing
+
+    [Parameter()]
+    [switch]
+    $push=$false #push to devops nightly feed
+)
+
+$projPath = '..\Our.Umbraco.MaintenanceMode\Our.Umbraco.MaintenanceMode.csproj';
+
+
+if ($version.IndexOf('-') -ne -1) {
+    Write-Host "Version shouldn't contain a - (remember version and suffix are seperate)"
+    exit
+}
+
+$fullVersion = $version;
+
+if (![string]::IsNullOrWhiteSpace($suffix)) {
+   $fullVersion = -join($version, '-', $suffix)
+}
+
+$majorFolder = $version.Substring(0, $version.LastIndexOf('.'))
+
+$outFolder = ".\$majorFolder\$version\$fullVersion"
+if (![string]::IsNullOrWhiteSpace($suffix)) {
+    $suffixFolder = $suffix;
+    if ($suffix.IndexOf('.') -ne -1) {
+        $suffixFolder = $suffix.substring(0, $suffix.indexOf('.'))
+    }
+    $outFolder = ".\$majorFolder\$version\$version-$suffixFolder\$fullVersion"
+}
+
+"----------------------------------"
+Write-Host "Version  :" $fullVersion
+Write-Host "Config   :" $env
+Write-Host "Folder   :" $outFolder
+"----------------------------------"; ""
+
+dotnet restore ..
+
+dotnet pack $projPath --no-restore -c $env -o $outFolder /p:ContinuousIntegrationBuild=true,version=$fullVersion 
+
+""; "##### Copying to LocalGit folder"; "----------------------------------" ; ""
+XCOPY "$outFolder\*.nupkg" "C:\Source\localgit" /Q /Y 
+
+if ($push) {
+    ""; "##### Pushing to our nighly package feed"; "----------------------------------" ; ""
+    .\nuget.exe push "$outFolder\*.nupkg" -ApiKey AzureDevOps -src https://pkgs.dev.azure.com/jumoo/Public/_packaging/nightly/nuget/v3/index.json
+    
+    Remove-Item ".\last-push-*" 
+    Out-File -FilePath ".\last-push-$fullVersion.txt" -InputObject $fullVersion
+}
+
+Write-Host "Packaged : $fullVersion"
+
+Remove-Item ".\last-build-*" 
+Out-File -FilePath ".\last-build-$fullVersion.txt" -InputObject $fullVersion
