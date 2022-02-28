886dab730dc6dccd1a0bc116457b3681d05043df
Aaron Sadler <aaron@aaronsadler.co.uk>
2022-02-28 20:58:56 +0000

    Added API endpoint to toggle maintenance mode

---
+++ Our.Umbraco.MaintenanceMode/Controllers/MaintenanceModeApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Our.Umbraco.MaintenanceMode.Interfaces;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Our.Umbraco.MaintenanceMode.Controllers
+{
+    public class MaintenanceModeApiController : UmbracoApiController
+    {
+        private readonly IMaintenanceModeService _maintenanceModeService;
+        private readonly Configurations.MaintenanceModeSettings _maintenanceModeSettings;
+
+        public MaintenanceModeApiController(IMaintenanceModeService maintenanceModeService, IOptions<Configurations.MaintenanceModeSettings> maintenanceModeSettings)
+        {
+            _maintenanceModeService = maintenanceModeService;
+            _maintenanceModeSettings = maintenanceModeSettings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Toggle(string apiKey)
+        {
+            if (_maintenanceModeSettings.EnableApi &&
+                _maintenanceModeSettings.ApiKey == apiKey &&
+                (!string.IsNullOrEmpty(_maintenanceModeSettings.ApiKey) ||
+                !string.IsNullOrWhiteSpace(_maintenanceModeSettings.ApiKey)))
+            {
+                var isEnabled = _maintenanceModeService.IsInMaintenanceMode;
+                _maintenanceModeService.ToggleMaintenanceMode(!isEnabled);
+
+                return Accepted();
+            }
+
+            return Unauthorized();
+        }
+    }
+}
