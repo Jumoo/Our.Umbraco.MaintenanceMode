166f6f99638ae21b012802837ac389f1b3994e0b
Donald Nairn <DONALD328@bailliegifford.com>
2023-04-06 22:25:08 +0100

    Add a nuspec and update AssemblyInfo.cs with details for nuget package.

--- Our.Umbraco.MaintenanceModeV8/Properties/AssemblyInfo.cs
+++ Our.Umbraco.MaintenanceModeV8/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Our.Umbraco.MaintenanceModeV8")]
+[assembly: AssemblyTitle("Our.Umbraco.MaintenanceMode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Our.Umbraco.MaintenanceModeV8")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Kevin Jump,Aaron Sadler")]
+[assembly: AssemblyProduct("Our.Umbraco.MaintenanceMode")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
