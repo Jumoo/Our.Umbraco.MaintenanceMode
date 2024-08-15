f9c55dfb6eebdd7f350de515e4a03115c015dbae
henryjump <140804846+henryjump@users.noreply.github.com>
2024-08-15 14:50:45 +0100

    use role to work out load balancing

--- Our.Umbraco.MaintenanceMode/Factories/StorageProviderFactory.cs
+++ Our.Umbraco.MaintenanceMode/Factories/StorageProviderFactory.cs
@@ -1,25 +1,25 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System;
+using Microsoft.Extensions.Options;
 using Our.Umbraco.MaintenanceMode.Configurations;
 using Our.Umbraco.MaintenanceMode.Providers;
-using System;
-using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Sync;
 
 namespace Our.Umbraco.MaintenanceMode.Factories
 {
     public class StorageProviderFactory : IStorageProviderFactory
     {
         private readonly Configurations.MaintenanceModeSettings _maintenanceModeSettings;
-        private readonly IOptions<GlobalSettings> _globalSettings;
         private readonly IServiceProvider _serviceProvider;
+        private readonly IServerRoleAccessor _serverRoleAccessor;
 
         public StorageProviderFactory(
             IOptions<Configurations.MaintenanceModeSettings> maintenanceModeSettings,
-            IOptions<GlobalSettings> globalSettings,
-            IServiceProvider serviceProvider)
+            IServiceProvider serviceProvider,
+            IServerRoleAccessor serverRoleAccessor)
         {
             _maintenanceModeSettings = maintenanceModeSettings.Value;
-            _serviceProvider = serviceProvider; 
-            _globalSettings = globalSettings;
+            _serviceProvider = serviceProvider;
+            _serverRoleAccessor = serverRoleAccessor;
         }
 
         //public StorageMode StorageMode => this._maintenanceModeSettings?.StorageMode ?? StorageMode.FileSystem;
@@ -27,21 +27,12 @@ namespace Our.Umbraco.MaintenanceMode.Factories
         {
             get
             {
-                if (_maintenanceModeSettings?.StorageMode is not null)
+                return _maintenanceModeSettings?.StorageMode ?? _serverRoleAccessor.CurrentServerRole switch
                 {
-                    // if it's been explicitly set in config
-                    // this is particularly useful for testing (!)
-                    return _maintenanceModeSettings.StorageMode;
-                }
-                else if (_globalSettings.Value.MainDomLock is not "MainDomSemaphoreLock") 
-                {
-                    // otherwise detect whether Umbraco is running in a distributed environment, if so we can't rely on file system/application scope
-                    // by default Umbraco runs under the semaphore lock
-                    // see https://docs.umbraco.com/umbraco-cms/fundamentals/setup/server-setup/load-balancing/azure-web-apps#host-synchronization
-                    return StorageMode.Database;
-                }
-                
-                return StorageMode.FileSystem;
+                    // check server role to see if umbraco thinks it's load balanced
+                    ServerRole.Subscriber or ServerRole.SchedulingPublisher => StorageMode.Database,
+                    _ => StorageMode.FileSystem,
+                };
             }
         }
 
